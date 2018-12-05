6548306257733b5535facc5e9a8c8d6f86eedcd3
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-12-05 14:42:46 -0600

    working compatibility logic, but scores coming back as strings from server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,26 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
+        //push user input to friends API
         friendsData.push(req.body);
+
+        // match user with most compatible friend in API
+        let userScores = req.body.scores;
+        // Go through each friend object in API array and reduce to one object, the friendMatch 
+        let friendMatch = friendsData.reduce((ACC, CURR) => {
+            // Within each friend object, go through each scores property and return one absolute value that reflects the sum of their score differences
+            let scoresCompatibility = userScores.reduce((acc, curr, idx) => 
+                acc + Math.abs(curr - CURR.scores[idx]), 0);
+                
+            if (scoresCompatibility < ACC.scoresCompatibility) {
+                return {
+                    friendMatch: CURR,
+                    scoresCompatibility: scoresCompatibility
+                }
+            } else return ACC;
+        }, {scoresCompatibility: Infinity});
         
+        res.json(friendMatch);
+
     });
 }
